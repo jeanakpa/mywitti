f220343afad46541b7e5f6301524f86fc41ca2e3
jeanakpa <122117356+jeanakpa@users.noreply.github.com>
2025-05-15 13:11:08 +0000

    Delete app directory

--- app/config.py
+++
@@ -1,7 +0,0 @@
-# app/config.py
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost/mywitti")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
--- app/routes/user_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-user_bp = Blueprint('user', __name__)
-
-@user_bp.route("/users", methods=["GET"])
-def get_users():
-    return {"message": "Liste des utilisateurs"}
